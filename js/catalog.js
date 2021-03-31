248ceaaa019a0327065e117a8e4f5bc5826da859
zhetic <zhetic@gmail.com>
2021-03-31 23:56:36 +0300

    added catalog

--- js/catalog.js
+++ js/catalog.js
@@ -4,25 +4,23 @@ function load() {
     .then(function (res) {
         var table = document.getElementById("titles");
         res.forEach(manga => {
-            var row = document.createElement("tr");
-
-            var cover = document.createElement("td");
-            cover.className = "cover";
-            cover.appendChild(Object.assign(
+            var block = document.createElement("div");
+            block.className = "block";
+            block.appendChild(Object.assign(
+                document.createElement("a"),
+                {href: "title.html?manga="+manga["id"]}
+            ))
+            .appendChild(Object.assign(
                 document.createElement("img"),
                 {className: "thumbnail", src: "img/covers/"+manga["id"]+".jpg"}
             ));
 
-            var desc = document.createElement("td");
-            desc.className = "description";
-            desc.appendChild(document.createElement("p"))
-            .appendChild(Object.assign(
-                document.createElement("b"),
-                {textContent: manga["title"]}
-            ));
+            var desc = document.createElement("div");
+            desc.className = "desc";
+            block.appendChild(desc);
             desc.appendChild(Object.assign(
-                document.createElement("p"),
-                {textContent: "Scanlation: ?"}
+                document.createElement("h3"),
+                {className: "title", textContent: manga["title"]}
             ));
             desc.appendChild(Object.assign(
                 document.createElement("p"),
@@ -36,14 +34,7 @@ function load() {
                 document.createElement("p"),
                 {textContent: "Last Update: ?"}
             ));
-            desc.appendChild(Object.assign(
-                document.createElement("a"),
-                {href: "title.html?manga="+manga["id"], textContent: "Chapters"}
-            ));
-
-            row.appendChild(cover);
-            row.appendChild(desc);
-            table.appendChild(row);
+            table.appendChild(block);
         });
     });
 }
